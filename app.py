b7e40e305e018e7f2911b8265a617a3ae087f7dc
Borglin <borglz1@medtronic.com>
2020-11-21 14:27:29 -0800

    Developing Flask page to show weather analysis

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+import os
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#Access the SQLite database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+#reflect database into our classes
+Base = automap_base()
+
+#reflect the database
+Base.prepare(engine, reflect=True)
+
+#Create a variable for each of the classes
+Measurement = Base.classes.Measurement
+Station = Base.classes.Station
+
+# Create session link from Python to our database
+session = Session(engine)
+
+os.environ['PYTHONPATH'] = os.getcwd()
+
+# define Flask app
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
-def maths():
-    return '4, 6, 8'    
+
+# Define the welcome route
+@app.route("/")
+
+# Set up routing information
+def welcome():
+    return(
+        '''
+        Welcome to the Climate Analysis API!
+        Available Routes:
+        /api/v1.0/precipitation
+        /api/v1.0/stations
+        /api/v1.0/tobs
+        /api/v1.0/temp/start/end
+        ''')
+
